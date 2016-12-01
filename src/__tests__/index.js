db31ddaa6d2a244fd6107123634a361c74a9cbf2
Tom Hicks <tom.hicks@bluespeckfinancial.co.uk>
2016-12-01 14:53:33 +0000

    Add mutative mode
    It is possible that prototype delegation could cause odd differences in
    behaviour depending on who is accessing which object reference. Some code
    may be referencing the source object, and some the target.
    
    This isn't often a problem but can be, so we allow a mutative mode so
    we can mutate the object directly and affect the source object.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,147 +1,175 @@
+/* eslint max-nested-callbacks: 0 */
+
 const {expect} = require("chai")
 const sinon = require("sinon")
 
-const whatsGoingOn = require("../")
+const marvin = require("../")
 
-describe("marvin", function() {
-  describe("with default options", () => {
-    describe("retaining function binding behaviour", () => {
-      const tom = whatsGoingOn({
-        name: "Tom",
-        sayName() {
-          return "Hello " + this.name
-        },
+const modes = [
+  {name: "delegation mode", fn: marvin},
+  {name: "mutative mode", fn: marvin.mutative},
+]
+
+modes.map(({name: mode, fn: whatsGoingOn}) => {
+  describe(`marvin (${mode})`, function() {
+    describe("with default options", () => {
+      describe("retaining function binding behaviour", () => {
+        const tom = whatsGoingOn({
+          name: "Tom",
+          sayName() {
+            return "Hello " + this.name
+          },
+        })
+
+        it("should preserve the this of the method", () => {
+          expect(tom.sayName()).to.equal("Hello Tom")
+        })
+
+        it("should not maintain a this if unbound", () => {
+          const unbound = tom.sayName
+          expect(unbound()).to.equal("Hello undefined")
+        })
+
+        it("should allow this binding", () => {
+          const alan = {name: "Alan"}
+          const sayHiAlan = tom.sayName.bind(alan)
+          expect(sayHiAlan()).to.equal("Hello Alan")
+        })
       })
 
-      it("should preserve the this of the method", () => {
-        expect(tom.sayName()).to.equal("Hello Tom")
+      describe("retaining property access behaviour", () => {
+        const simpleObject = whatsGoingOn({foo: "bar"})
+
+        it("should retain normal property access behaviour", () => {
+          expect(simpleObject.foo).to.equal("bar")
+        })
+
+        it("should allow normal property setting", () => {
+          simpleObject.baz = "qux"
+          expect(simpleObject.baz).to.equal("qux")
+        })
       })
 
-      it("should not maintain a this if unbound", () => {
-        const unbound = tom.sayName
-        expect(unbound()).to.equal("Hello undefined")
+      it("should let function calls through", () => {
+        const obj = {
+          method() {
+            return 5
+          },
+        }
+        const loggedObject = whatsGoingOn(obj)
+        sinon.spy(loggedObject, "method")
+
+        const result = loggedObject.method(1, 2, 3)
+
+        sinon.assert.calledWith(loggedObject.method, 1, 2, 3)
+        expect(result).to.equal(5)
       })
 
-      it("should allow this binding", () => {
-        const alan = {name: "Alan"}
-        const sayHiAlan = tom.sayName.bind(alan)
-        expect(sayHiAlan()).to.equal("Hello Alan")
+      it("should skip unwritable properties", () => {
+        const obj = Object.freeze({a() {}})
+        expect(() => whatsGoingOn(obj)).to.not.throw()
       })
     })
 
-    describe("retaining property access behaviour", () => {
-      const simpleObject = whatsGoingOn({foo: "bar"})
+    describe("when a logging function is passed", () => {
+      let logSpy
 
-      it("should retain normal property access behaviour", () => {
-        expect(simpleObject.foo).to.equal("bar")
+      beforeEach(() => {
+        logSpy = sinon.spy()
+        const loggedObject = whatsGoingOn({
+          jamie() {
+            return 5
+          },
+
+          another() {},
+        }, {log: logSpy})
+
+        loggedObject.jamie(1, 2, 3)
+        loggedObject.another()
+        loggedObject.jamie(4, 5, 6)
       })
 
-      it("should allow normal property setting", () => {
-        simpleObject.baz = "qux"
-        expect(simpleObject.baz).to.equal("qux")
+      it("should call the logging function for each method call", () => {
+        sinon.assert.calledThrice(logSpy)
+      })
+
+      it("should call the logging function with name, arguments and returnValue", () => {
+        expect(logSpy.firstCall.args[1]).to.eql({
+          name: "jamie", call: {args: [1, 2, 3], returnValue: 5},
+        })
+
+        expect(logSpy.secondCall.args[1]).to.eql({
+          name: "another", call: {args: [], returnValue: undefined},
+        })
+
+        expect(logSpy.thirdCall.args[1]).to.eql({
+          name: "jamie", call: {args: [4, 5, 6], returnValue: 5},
+        })
+      })
+
+      it("should include the list of calls to that method as the third parameter", () => {
+        expect(logSpy.firstCall.args[2]).to.eql([
+          {args: [1, 2, 3], returnValue: 5},
+        ])
+
+        expect(logSpy.thirdCall.args[2]).to.eql([
+          {args: [1, 2, 3], returnValue: 5},
+          {args: [4, 5, 6], returnValue: 5},
+        ])
+      })
+
+      it("should include the list of calls to all methods as the fourth parameter", () => {
+        expect(logSpy.firstCall.args[3]).to.eql([
+          {name: "jamie", call: {args: [1, 2, 3], returnValue: 5}},
+        ])
+
+        expect(logSpy.secondCall.args[3]).to.eql([
+          {name: "jamie", call: {args: [1, 2, 3], returnValue: 5}},
+          {name: "another", call: {args: [], returnValue: undefined}},
+        ])
+
+        expect(logSpy.thirdCall.args[3]).to.eql([
+          {name: "jamie", call: {args: [1, 2, 3], returnValue: 5}},
+          {name: "another", call: {args: [], returnValue: undefined}},
+          {name: "jamie", call: {args: [4, 5, 6], returnValue: 5}},
+        ])
       })
     })
 
-    it("should let function calls through", () => {
-      const obj = {method: sinon.stub().returns(5)}
-      const loggedObject = whatsGoingOn(obj)
+    describe("when a logging function and an object name is passed", () => {
+      let logSpy
 
-      const result = loggedObject.method(1, 2, 3)
+      beforeEach(() => {
+        logSpy = sinon.spy()
+        const loggedObject = whatsGoingOn({
+          jamie() {
+            return 5
+          },
+        }, {
+          log: logSpy,
+          objectName: "myObject",
+        })
 
-      sinon.assert.calledWith(obj.method, 1, 2, 3)
-      expect(result).to.equal(5)
-    })
+        loggedObject.jamie(1, 2, 3)
+      })
 
-    it("should skip unwritable properties", () => {
-      const obj = Object.freeze({a() {}})
-      expect(() => whatsGoingOn(obj)).to.not.throw()
+      it("should include the object name with the log call", () => {
+        expect(logSpy.firstCall.args[0]).to.eql("myObject")
+      })
     })
   })
+})
 
-  describe("when a logging function is passed", () => {
-    let logSpy
-
-    beforeEach(() => {
-      logSpy = sinon.spy()
-      const loggedObject = whatsGoingOn({
-        jamie() {
-          return 5
-        },
-
-        another() {},
-      }, {log: logSpy})
-
-      loggedObject.jamie(1, 2, 3)
-      loggedObject.another()
-      loggedObject.jamie(4, 5, 6)
-    })
-
-    it("should call the logging function for each method call", () => {
-      sinon.assert.calledThrice(logSpy)
-    })
-
-    it("should call the logging function with name, arguments and returnValue", () => {
-      expect(logSpy.firstCall.args[1]).to.eql({
-        name: "jamie", call: {args: [1, 2, 3], returnValue: 5},
-      })
-
-      expect(logSpy.secondCall.args[1]).to.eql({
-        name: "another", call: {args: [], returnValue: undefined},
-      })
-
-      expect(logSpy.thirdCall.args[1]).to.eql({
-        name: "jamie", call: {args: [4, 5, 6], returnValue: 5},
-      })
-    })
-
-    it("should include the list of calls to that method as the third parameter", () => {
-      expect(logSpy.firstCall.args[2]).to.eql([
-        {args: [1, 2, 3], returnValue: 5},
-      ])
-
-      expect(logSpy.thirdCall.args[2]).to.eql([
-        {args: [1, 2, 3], returnValue: 5},
-        {args: [4, 5, 6], returnValue: 5},
-      ])
-    })
-
-    it("should include the list of calls to all methods as the fourth parameter", () => {
-      expect(logSpy.firstCall.args[3]).to.eql([
-        {name: "jamie", call: {args: [1, 2, 3], returnValue: 5}},
-      ])
-
-      expect(logSpy.secondCall.args[3]).to.eql([
-        {name: "jamie", call: {args: [1, 2, 3], returnValue: 5}},
-        {name: "another", call: {args: [], returnValue: undefined}},
-      ])
-
-      expect(logSpy.thirdCall.args[3]).to.eql([
-        {name: "jamie", call: {args: [1, 2, 3], returnValue: 5}},
-        {name: "another", call: {args: [], returnValue: undefined}},
-        {name: "jamie", call: {args: [4, 5, 6], returnValue: 5}},
-      ])
-    })
+describe("specific mode characteristics", function() {
+  it("normal mode should return the same object as passed in", () => {
+    const source = {}
+    const proxy = marvin(source)
+    expect(proxy).not.to.equal(source)
   })
 
-  describe("when a logging function and an object name is passed", () => {
-    let logSpy
-
-    beforeEach(() => {
-      logSpy = sinon.spy()
-      const loggedObject = whatsGoingOn({
-        jamie() {
-          return 5
-        },
-      }, {
-        log: logSpy,
-        objectName: "myObject",
-      })
-
-      loggedObject.jamie(1, 2, 3)
-    })
-
-    it("should include the object name with the log call", () => {
-      expect(logSpy.firstCall.args[0]).to.eql("myObject")
-    })
+  it("mutative mode should return the same object as passed in", () => {
+    const source = {}
+    const proxy = marvin.mutative(source)
+    expect(proxy).to.equal(source)
   })
 })
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@
 
 const inspect = require("./inspect")
 
-function marvin(object, {log, objectName} = {}) {
-  const proxy = Object.create(object)
+const DISTANT_LOVER = "proto"
+const AINT_NOTHIN_LIKE_THE_REAL_THING = "mutate"
+
+function marvin(object, {log, objectName, mode = DISTANT_LOVER} = {}) {
+  if (Object.isFrozen(object)) return object
+
+  const proxy = mode === DISTANT_LOVER ? Object.create(object) : object
 
   proxy.__marvin = []
   proxy.__marvinInspect = inspect(proxy)
 
   for (const name in object) {
     if (typeof object[name] === "function" && Object.getOwnPropertyDescriptor(object, name).writable) {
+      const originalFunction = object[name]
+
       proxy[name] = function(...args) {
-        const returnValue = object[name].apply(this, args)
+        const returnValue = originalFunction.apply(this, args)
 
         const functionCall = {args, returnValue}
         proxy[name].__marvin.push(functionCall)
@@ -35,6 +42,10 @@ function marvin(object, {log, objectName} = {}) {
   }
 
   return proxy
+}
+
+marvin.mutative = function(object, options) {
+  return marvin(object, Object.assign({}, options, {mode: AINT_NOTHIN_LIKE_THE_REAL_THING}))
 }
 
 module.exports = marvin
